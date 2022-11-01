337755446142388e28935b55dc9a1e2e5969c824
ViniGonbini <1605269@cstj.qc.ca>
2022-11-01 15:55:01 -0400

    Ouverture sur login

--- app/src/main/java/com/babileux/andromia/presentation/ui/LoginActivity.kt
+++ app/src/main/java/com/babileux/andromia/presentation/ui/LoginActivity.kt
@@ -2,11 +2,30 @@ package com.babileux.andromia.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import com.babileux.andromia.R
+import com.babileux.andromia.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private val viewModel: LoginViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_login)
+
+
+
+        binding.btnConnecter.setOnClickListener {
+            Toast.makeText(this, "LE bouton a été cliquer", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/babileux/andromia/presentation/ui/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.babileux.andromia.presentation.ui
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class LoginViewModel(application: Application): AndroidViewModel(application) {
+
+}
