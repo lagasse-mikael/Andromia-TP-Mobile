ffa26da8d119757d30556004a2fbf0faa1690a75
Akelouche-cstj <1643859@cstj.qc.ca>
2022-11-15 16:12:57 -0500

    Creatures on progress

--- app/src/main/java/com/babileux/andromia/data/datasources/CreatureDataSource.kt
+++ app/src/main/java/com/babileux/andromia/data/datasources/CreatureDataSource.kt
@@ -3,6 +3,7 @@ package com.babileux.andromia.data.datasources
 import com.babileux.andromia.core.Constants
 import com.babileux.andromia.domain.models.Creature
 import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.json.responseJson
 import com.github.kittinunf.result.Result
@@ -16,10 +17,10 @@ class CreatureDataSource {
     private val json = Json { ignoreUnknownKeys = true }
 
 
-    suspend fun retrieveCreatureExplorer(header : String) : List<Creature> {
+    suspend fun retrieveCreatureExplorer(accessToken: String) : List<Creature> {
         return withContext(Dispatchers.IO) {
 
-            val (_, _, result) = Constants.BaseURL.CREATURES.httpGet().header().responseJson()
+            val (_, _, result) = Constants.BaseURL.CREATURES.httpGet().authentication().bearer(accessToken).responseJson()
             when(result) {
                 is Result.Success -> {
                     return@withContext json.decodeFromString(result.value.content)
